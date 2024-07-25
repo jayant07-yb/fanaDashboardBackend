03a22de28a9d8ff0e1b9a753584c3a6bf7977f42
Jayant Anand <jayantanand2001@gmail.com>
2024-07-26 00:42:13 +0530

    Added basic flask changes

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <WiFi.h>
+
+const char* ssid = "comp";
+const char* password = "P90962u$";
+
+void setup() {
+  pinMode(2, OUTPUT); // Use GPIO 2 for the built-in LED
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+  digitalWrite(2, HIGH); // Turn the LED on (GPIO 2)
+  delay(1000); // Wait for a second
+  digitalWrite(2, LOW); // Turn the LED off (GPIO 2)
+  delay(1000); // Wait for a second
+}
