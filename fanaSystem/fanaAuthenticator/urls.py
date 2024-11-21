49142e3284a13eb81814331e372165935ccda3f7
Ayush Barman <barmanayush2980@gmail.com>
2024-11-22 00:02:38 +0530

    Cleaned up code

--- fanaSystem/fanaAuthenticator/urls.py
+++ fanaSystem/fanaAuthenticator/urls.py
@@ -5,10 +5,9 @@ from .views import CustomTokenObtainPairView, VerifyOtp , ProtectedRouteCheck,Ad
 app_name = 'fanaAuthenticator'
 
 urlpatterns = [
-    path('api/send-otp', CustomTokenObtainPairView.as_view(), name='sent-otp'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='custom_token_obtain_pair'),
     path('api/verify-otp', VerifyOtp.as_view(), name='verify-otp'),
     path('api/protected' , ProtectedRouteCheck.as_view() ,name="protected_route_check")  ,
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='custom_token_obtain_pair'),
     path('api/add_device/', AddDeviceView.as_view(), name='add_device'),
     path('api/remove_device/', RemoveDeviceView.as_view(), name='remove_device'),
     path('handle_customer_order/', handle_customer_order, name='handle_customer_order')
