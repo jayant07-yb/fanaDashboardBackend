b957d1632c30848db33cf816b2077cdcaa941520
Jayant Anand <jayantanand2001@gmail.com>
2025-02-01 05:13:06 +0530

    Corrected the url for non wsl related stuff

--- fanaSystem/fanaSystem/settings.py
+++ fanaSystem/fanaSystem/settings.py
@@ -30,7 +30,7 @@ AUTH_SERVER_IP = os.getenv("AUTH_SERVER_IP", DEFAULT_SERVER_HOST)
 PUBLIC_IP = "internals.getfana.com"
 
 # Construct the full URL for the authentication endpoint
-BASE_URL = f"https://{PUBLIC_IP if PUBLIC_IP else 'localhost'}:8000"
+BASE_URL = f"https://{PUBLIC_IP if PUBLIC_IP else 'localhost:8000'}"
 AUTH_SERVER_LOGIN_URL = f"{BASE_URL}/fanaAuthenticator/api/token/"
 WSL_SERVER_URL = f"https://{PUBLIC_IP if PUBLIC_IP else 'localhost'}:{8001 if PUBLIC_IP else 8000}/ws/dashboard/"
 SEND_ORDER_TO_DASHBOARD_URL =  f"{BASE_URL}/fanaDashboard/receiveOrder/"
