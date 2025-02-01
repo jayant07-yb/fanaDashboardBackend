8a3559bc53d2f7e553767b1e3dcf07b07375cf3e
Jayant Anand <jayantanand2001@gmail.com>
2025-02-01 19:52:31 +0530

    Fixed the wss access

--- fanaSystem/fanaSystem/settings.py
+++ fanaSystem/fanaSystem/settings.py
@@ -32,7 +32,7 @@ PUBLIC_IP = "internals.getfana.com"
 # Construct the full URL for the authentication endpoint
 BASE_URL = f"https://{PUBLIC_IP if PUBLIC_IP else 'localhost:8000'}"
 AUTH_SERVER_LOGIN_URL = f"{BASE_URL}/fanaAuthenticator/api/token/"
-WSL_SERVER_URL = f"https://{PUBLIC_IP if PUBLIC_IP else 'localhost'}:{8001 if PUBLIC_IP else 8000}/ws/dashboard/"
+WSL_SERVER_URL = f"https://{PUBLIC_IP if PUBLIC_IP else 'localhost:8000'}/ws/dashboard/"
 SEND_ORDER_TO_DASHBOARD_URL =  f"{BASE_URL}/fanaDashboard/receiveOrder/"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
